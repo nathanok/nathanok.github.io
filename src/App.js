83f0c2b2a50e7a02f4332d8527172ec207e92b81
Nathan <cpjsmith1@gmail.com>
2020-04-08 13:11:09 -0400

    made blank page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './nathan.jpg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-My name is Nathan and I am going to master react!
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/nathanok"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check me Out
-        </a>
-      </header>
+     
     </div>
   );
 }
