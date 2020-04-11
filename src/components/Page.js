d00d313bb6a97aa8b3d87c422749fe74fb67bbac
Nathan <cpjsmith1@gmail.com>
2020-04-11 13:01:33 -0400

    convert html to javascript

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,10 +6,29 @@ class Page extends React.Component {
     render() {
         return (
         <div className="Page">
+            
+           
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>"
+
+document.write(info)
             </div>
+            
+            
         );
         
         }
+    
+    
     }
 
 
