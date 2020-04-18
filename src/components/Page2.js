d4868d6d053a4b4400623b7fdb2fab028ee19d34
Nathan <cpjsmith1@gmail.com>
2020-04-17 20:04:07 -0400

    first page added

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -16,7 +16,7 @@ class Page extends React.Component {
 <body>
 
 <h1>Nathan Williams</h1>
-<a href="Home"> <h2> Home/ </h2> </a>
+<h2> Home/ </h2>
 <h2> Gallery/ </h2>
 <h2> Videos </h2>
 <p>My first paragraph.</p>
