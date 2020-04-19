f383980fecf1e85f13b80245c0abfd407cebc7f5
Nathan <cpjsmith1@gmail.com>
2020-04-18 21:05:12 -0400

    Gallery Page done

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -21,6 +21,11 @@ class Gallery extends React.Component {
 <div class="mypic"></div>
             <div class="back"></div>
             <div class="img"></div>
+             <div class="img2"></div>
+             <div class="img3"></div>
+             <div class="img4"></div>
+               <div class="img5"></div>
+               <div class="img6"></div>
 
 </body>
 
