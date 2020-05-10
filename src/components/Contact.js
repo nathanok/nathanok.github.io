b7cae1c0273c454756541c453718dc212b0c6a0b
Nathan <cpjsmith1@gmail.com>
2020-05-10 19:15:53 -0400

    added new page will need to fix later

---
+++ src/components/Contact.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "../css/Contact.css";
+
+
+class Contact extends React.Component {
+ 
+    render() {
+        return (
+        <div className="Page">
+  
+           
+
+<body>
+
+<h1>Nathan Williams</h1>
+
+<p>Welcome to my Portfolio</p>
+                
+<div class="mypic"></div>
+            
+     <div class="Aboutme">
+            <div class="text">
+            <h4>About Me </h4>
+            <p> I attended Lehman College to obtain a Bachelor degree in Computer Graphics and Imaging. Ever since my earlier years I have been interested in the underlying processes that go into developing video games. I was particularly engrossed in animation and character modeling and have worked to constantly improve my aptitude in those areas. </p>
+            <p> Please go to the website below for my Resume and contact information </p> <h5> https://www.linkedin.com/in/nathan-williams-6b57a712b/detail/contact-info/ </h5>
+            </div>
+            </div>
+
+</body>
+
+
+
+            </div>
+            
+            
+        );
+        
+        }
+    
+    
+    }
+    //content:url(".././images/picof.jpg");
+
+
+export default Contact;
+//background-image: url(".././images/A422roomfullview.jpg");
