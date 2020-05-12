853648adca8ca679d72f62b1f0b059288fc33ce4
Nathan <cpjsmith1@gmail.com>
2020-05-12 15:52:18 -0400

    Added Button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../css/Contact.css";
 import axios from 'axios';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 
 class Contact extends React.Component{
@@ -26,13 +27,24 @@ class Contact extends React.Component{
         <p> Contact Page in Progress</p>
                 </div>
             <div class="word">
-            <Form>
-            <Form.Group>
-            <Form.Label htmlFor="full-name">Full Name</Form.Label>
-            <Form.Control id="full-name" name="name" type="text" value={this.state.name} />
-           
+                <Form>
             
-            </Form.Group>
+         <Form.Group>
+            <Form.Label htmlFor="full-name">Full Name</Form.Label>
+            <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+             </Form.Group>
+            
+             <Form.Group>
+            <Form.Label htmlFor="Email">Email</Form.Label>
+            <Form.Control id="Email" name="Email" type="email" value={this.state.email} onChange={this.handleChange} />
+             </Form.Group>
+            
+             <Form.Group>
+            <Form.Label htmlFor="Message">Message</Form.Label>
+            <Form.Control id="Message" name="Message" as="textarea"rows="3" value={this.state.message} onChange={this.handleChange} />
+             </Form.Group>
+            
+                    <Button>Send</Button>
             
             </Form>
                 </div>
